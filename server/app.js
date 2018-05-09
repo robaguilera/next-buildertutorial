5c94759b638050e7ddb69aa21d63cff2a5ca1be8
Rob <robert@aplos.com>
2018-05-08 20:10:07 -0700

    stuck

---
+++ server/app.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import next from 'next';
+
+const port = process.env.PORT || 8000;
+const ROOT_URL = process.env.ROOT_URL || `http://localhost:${port}`;
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  server.get('/', (req, res) => {
+    // res.send('My express server');
+  });
+
+  server.get('*', (req, res) => handle(req, res));
+
+  server.listen(port, err => {
+    if (err) throw err;
+    console.log(`> Ready on ${ROOT_URL}`); // eslint-disable-line no-console
+  });
+});
